d825a6aa53cdcbd0ae64b08a29868a012e81612a
Nockzb <132928494+Nockzb@users.noreply.github.com>
2023-12-21 16:39:38 +0100

    LA PUTA MADRE

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
+import { GifsService } from '../../services/gifs.service';
+
 
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.css']
 })
+
 export class CardListComponent {
 
+  @Input()
+  public resultadosGifs: Gif[] = [];
+  
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,23 +1,26 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-  public listadoGifs: Gif[] = []
-  private _historialEtiquetas: string[] = [];
   private apiKey: string = 'IsQyHrWS9GjZMX0zOTUrPQmg7wB5I6Zf';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
-  
+
+  public listadoGifs: Gif[] = []
+  private _historialEtiquetas: string[] = [];  
+
   constructor(private http: HttpClient) { }
 
+  // Método para obtener una copia de _historialEtiquetas
   get historialEtiquetas() {
     return [...this._historialEtiquetas]
   }
 
-  // Metodo para organizar el historial de busquedas
+  // Método para organizar el historial de búsquedas
   organizarHistorial(etiquet: string): void {
     if (etiquet.length == 0 || etiquet == null) {
 
@@ -25,16 +28,19 @@ export class GifsService {
       if (this._historialEtiquetas.includes(etiquet)) {
         this._historialEtiquetas = this._historialEtiquetas.filter(etiq => etiq != etiquet)
         this._historialEtiquetas.unshift(etiquet);
-        
+
       } else {
         this._historialEtiquetas.unshift(etiquet);
       }
     }
 
+    // Se muestran siempre los últimos 10 elementos
     this._historialEtiquetas = this._historialEtiquetas.splice(0, 10);
   }
 
-  // Metodo para realizar busquedas
+  // Método para realizar búsquedas
+  // y generar un objeto de tipo <SearchResponse>
+  // con los resultados
   buscarEtiqueta(etiqueta: string): void {
     const etiquetaTrim = etiqueta.toLowerCase().trim();
     this.organizarHistorial(etiquetaTrim);
@@ -44,9 +50,8 @@ export class GifsService {
       .set('q', etiqueta)
       .set('limit', 10)
 
-    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params }).subscribe(resp => {
-      console.log(resp);   
-      this.listadoGifs = resp.data   
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params }).subscribe(resp => {      
+      this.listadoGifs = resp.data      
     })
-  }  
+  }
 }
